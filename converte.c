3a5f4013974aeb75dd1faf6da4ee318474da30f7
Geovanna Nascimento <geogeovannarn@gmail.com>
2022-04-15 14:57:19 +0000

    implementando funções

--- converte.c
+++ converte.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main (void){
+    int n2, n1c;
+    char n1[9], operacao;
+
+    scanf("%s", n1);
+    n1c = strtol(n1, NULL, 10);
+
+    if (strcmp(n1, "S") == 0) {
+        printf("O programa foi encerrado!\n");
+    } else {
+        scanf(" %c", &operacao);
+        scanf("%d", &n2);
+        
+        printf("%d\n", n1c);
+
+        if(operacao == '+'){
+        }
+
+
+        printf("%sb\n%c\n%db\n=\n", n1, operacao, n2);
+    }
     return 0;
 }
