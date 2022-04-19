f4e3c8875fd1dcea6941d32a3fb14f2d84a32de6
geordtl <geogeovannarn@gmail.com>
2022-04-19 07:01:08 -0300

    adição de 0

--- converte.c
+++ converte.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main (void){
-    int j, k, s, i, numero1[9], numero2[9], n1c, n2c, n1q, n2q, contaDigitos;
+    int j, k, s, i, m, n, numero1[9], numero2[9], n1c, n2c, n1q, n2q, contaDigitos, aux = 0, aux2 = 0;
     char n1[9], n2[9], soma[9], sub[9], tempSoma = 0, tempSub, tempSub2, operacao;
 
     scanf("%8s", n1);
@@ -15,28 +15,17 @@ int main (void){
     	numero1[k] = 0;	
     }
     
-    for(i = 0; i <= n1c*10; i++){
-    	aux = n1c%10;
-    	n1c = n1c/10;
-    	
-    	numero1[i] = aux;
-    }
     
     for(s = (8-contaDigitos); s < 8; s++){
-    	numero1[s] = strtol(n1[s-(8-contaDigitos)]);
+    	numero1[s] = (int)n1[s-(8-contaDigitos)];
+    	
+    	if(numero1[s] == 48){
+    		numero1[s] = 0;
+    	}
+    	if(numero1[s] == 49){
+    		numero1[s] = 1;
+    	}
     }
-    
-    printf("%d\n", numero1[0]);
-    printf("%d\n", numero1[1]);
-    printf("%d\n", numero1[2]);
-    printf("%d\n", numero1[3]);
-    printf("%d\n", numero1[4]);
-    printf("%d\n", numero1[5]);
-    printf("%d\n", numero1[6]);
-    printf("%d\n", numero1[7]);
-    
-    int aux = 0;
-    int aux2 = 0;
     
     
     if (strcmp(n1, "S") == 0) {
@@ -47,14 +36,32 @@ int main (void){
         n2c = strtol(n2, NULL, 10);
         n2q = n2c;
         
-        for(j = 0; j <= n2c*10; j++){
-            aux2 = n2c%10;
-            n2c = n2c/10;
-            
-            numero2[j] = aux2;
-        }
-        
         if(operacao == '+'){
+        contaDigitos2 = strlen(n2);
+        
+	    for(m = 0; m < (8-contaDigitos2); m++){
+	    	numero2[m] = 0;	
+	    }
+	    
+	    for(n = (8-contaDigitos2); n < 8; n++){
+	    	numero2[n] = (int)n2[n-(8-contaDigitos2)];
+	    	
+	    	if(numero2[n] == 48){
+	    		numero2[n] = 0;
+	    	}
+	    	if(numero2[n] == 49){
+	    		numero2[n] = 1;
+	    	}
+	    }
+        	
+        	    printf("%d\n", numero2[0]);
+		    printf("%d\n", numero2[1]);
+		    printf("%d\n", numero2[2]);
+		    printf("%d\n", numero2[3]);
+		    printf("%d\n", numero2[4]);
+		    printf("%d\n", numero2[5]);
+		    printf("%d\n", numero2[6]);
+		    printf("%d\n", numero2[7]);
            /*printf("%db\n%c\n%db\n=\n", numero1, operacao, numero2);*/
         }
     }
