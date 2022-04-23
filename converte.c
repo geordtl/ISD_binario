7bf810415c4be639d575acd5ae043a57ec3771f8
geordtl <geogeovannarn@gmail.com>
2022-04-23 17:23:45 -0400

    terminando o código

--- converte.c
+++ converte.c
@@ -6,17 +6,17 @@ int main(void)
 {
 	/* variáveis */
 	/* índices */
-	int j, k, s, i, m, n, p, q, r, z, y, u, l, d,
+	int j, k, s, i, m, n, p, q, r, z, y, u, l, d, h,
 
 		/* auxiliares */
 		n1c, n2c, n1q, n2q, contaDigitos, contaDigitos2, tempSoma = 0, tempSub = 0, tempSub2 = 0, tempSub3 = 0,
 
-														 /* arrays */
+		/* arrays */
 		numero1[9], numero2[9],
 
-														 /* operações */
-		mod, soma, sub, sub2, potDec = 1, potHex = 1, totSoma[9], totSub[9], comp2, totComp2[9], n1Dec, n1Hex, soma1Dec = 0, soma1Hex = 0;
-	char n1[9], n1s[9], n1Sub[9], n2[9], n2s[9], operacao, somaChar[9], subChar[9], comp2C[9];
+		/* operações */
+		mod, mod2, soma, sub, sub2, potDec = 1, totSoma[9], totSub[9], comp2, totComp2[9], n1Dec, soma1Dec = 0, soma2Dec = 0;
+	char n1[9], n1s[9], n1Sub[9], n2[9], n2s[9], n2Sub[9], operacao, somaChar[9], subChar[9], comp2C[9];
 
 	scanf("%8s", n1);
 	n1q = strtol(n1, NULL, 10);
@@ -120,15 +120,20 @@ int main(void)
 				mod = n1c % 10;
 				n1c = n1c / 10;
 
+				mod2 = n2c % 10;
+				n2c = n2c / 10;
+
 				soma1Dec = soma1Dec + mod * potDec;
+				soma2Dec = soma2Dec + mod2 * potDec;
 				potDec = potDec * 2;
 			}
 
-			printf("%db (%di)\n%c\n%db\n=\n%db\n", converteN1, soma1Dec, operacao, converteN2, converteSoma);
+			printf("%db (%di)\n%c\n%db (%di)\n=\n%db\n", converteN1, soma1Dec, operacao, converteN2, soma2Dec, converteSoma);
 		}
 		if (operacao == '-')
 		{
 			/* implementa o complemento a 1 */
+			if((numero1[0] == 0 && numero2[0] == 0) || (numero1[0] == 1 && numero2[0] == 0)){
 			for (r = 0; r <= 7; r++)
 			{
 				if (numero2[r] == 1)
@@ -141,7 +146,6 @@ int main(void)
 				}
 			}
 
-			/* if(numero1[0] == 0 && numero2[0] == 0){
 				/* implementa o complemento a 2 */
 			for (z = 7; z >= 0; z--)
 			{
@@ -180,6 +184,59 @@ int main(void)
 				}
 				totComp2[z] = comp2;
 			}
+		}
+
+		if((numero1[0] == 0 && numero2[0] == 1) || (numero1[0] == 1 && numero2[0] == 1)){
+			/* tirando o complemento a 2 */
+			for (z = 7; z >= 0; z--){
+				if (z == 7)
+				{
+					if (numero2[7] == 0)
+					{
+						totComp2[z] = 1;
+						tempSub = 1;
+						z = 6;
+					}
+					else
+					{
+						totComp2[z] = 0;
+						tempSub = 0;
+						z = 6;
+					}
+				}
+				if (z <= 6 && z >= 0)
+				{
+					sub = numero2[z] - tempSub;
+				}
+				
+				switch (sub)
+				{
+				case 0:
+					sub = 0;
+					tempSub = 0;
+					totComp2[z] = sub;
+				break;
+				case 1:
+					sub = 1;
+					tempSub = 0;
+					totComp2[z] = sub;
+				break;
+				}
+			}
+			
+				/* tirando o complemento a 1 */
+				for (r = 0; r <= 7; r++)
+				{
+					if (totComp2[r] == 1)
+					{
+						totComp2[r] = 0;
+					}
+					else
+					{
+						totComp2[r] = 1;
+					}
+				}
+		}
 
 			/* soma os binários */
 			for (u = 7; u >= 0; u--)
@@ -212,12 +269,13 @@ int main(void)
 			for (y = 7; y >= 0; y--)
 			{
 				n1Sub[y] = numero1[y] + '0';
+				n2Sub[y] = numero2[y] + '0';
 				comp2C[y] = totComp2[y] + '0';
 				subChar[y] = totSub[y] + '0';
 			}
 
 			int converteN1 = strtol(n1Sub, NULL, 10);
-			int converteN2 = strtol(comp2C, NULL, 10);
+			int converteN2 = strtol(n2Sub, NULL, 10);
 			int converteSub = strtol(subChar, NULL, 10);
 
 			printf("%db\n%c\n%db\n=\n%db\n", converteN1, operacao, converteN2, converteSub);
